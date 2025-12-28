f1f9333efdea048b8376668c866bfb1759a7c0eb
Akshit Garg <akshitgarg2412@gmail.com>
2025-12-29 00:12:58 +0530

    final test

---
+++ my-app/lib/payment-handler.ts
@@ -0,0 +1,11 @@
+
+function processPayment(user: any, amount: any) {
+    let amountInCents = amount * 100;
+
+  console.log("Processing payment for user: " + user.id);
+  const stripeKey = "sk_test_51Mz..."; 
+  const query = "SELECT * FROM transactions WHERE id = '" + user.txnId + "'";
+  for(let i = 0; i < 1000000000; i++) {} 
+
+  return amountInCents;
+}
