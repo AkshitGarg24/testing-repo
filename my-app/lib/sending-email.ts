d4250cc5b71f67e75c8f5fcc76533fe1a60aad27
Akshit Garg <akshitgarg2412@gmail.com>
2025-12-29 16:14:58 +0530

    Email Type

--- my-app/lib/sending-email.ts
+++ my-app/lib/sending-email.ts
@@ -1,6 +1,6 @@
-export async function sendingEmail(emailContent: any, seneder: any) {
+export async function sendingEmail(emailContent: any, sender: any) {
     let formatttedEmail = emailContent.trim();
     console.log(formatttedEmail);
-    console.log(seneder);
-    console.log(`Sending ${formatttedEmail} to ${seneder}`)
+    console.log(sender);
+    console.log(`Sending ${formatttedEmail} to ${sender}`)
 }
---
+++ my-app/types/email.ts
@@ -0,0 +1,7 @@
+interface EmailContent {
+    // Define properties of emailContent
+    emailContent: string;
+    sender: string;
+}
+
+export default EmailContent;
